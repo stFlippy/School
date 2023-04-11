0bc6ae9b353b990391e8d4156ed403fa5229308e
Владимир <7572vova@mail.ru>
2023-04-11 18:24:44 +0400

    коммит....

--- VK-bot/main.py
+++ VK-bot/main.py
@@ -1,15 +1,19 @@
+import vk_api.bot_longpoll
+from vk_api import VkApi
+from auth_data import auth_key, group_id
 
-def load_pass ():
-    auth_key = ''
-    with open('config.ini', mode='r', encoding='utf8') as configs:
-        for line in configs:
-            auth_key = line
-            break
-    return auth_key
 
-def call ():
-    pass
+class EchoBot:
+
+    def __init__(self, key, id):
+        self.vk_src = VkApi(token=key)
+        self.bot = vk_api.bot_longpoll.VkBotLongPoll(self.vk_src, id)
+
+    def run(self, timeout=25):
+        for event in self.bot.listen():
+            print(event)
 
 
 if __name__ == '__main__':
-     pass
+    bot = EchoBot(auth_key, group_id)
+    bot.run()
