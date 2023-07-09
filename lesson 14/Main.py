cb241511772ca1f5eb4eebe9ab78adffe9f54c5c
Владимир <7572vova@mail.ru>
2023-07-09 19:08:06 +0400

    Намутил счетчик и один тест на него

--- lesson 14/Main.py
+++ lesson 14/Main.py
@@ -1,24 +1,41 @@
+class VsePoJope(Exception):
 
-class bowl_cntr:
-    def __init__(self):
+    def __str__(self):
+        return "Чекни входные данные"
+
+
+class BowlCntr:
+    def __init__(self, name):
+        self.name = name
         self.game_result = ""
         self.cnt = 0
 
     def get_score(self, inpt):
         self.game_result = inpt
 
-        for i, chr in enumerate(self.game_result):
-            if chr.isdigit:
-                self.cnt += int(chr)
-                continue
+        for i, symbol in enumerate(self.game_result):
+            try:
+                if symbol.isdigit():
+                    self.cnt += int(symbol)
+                    continue
 
-            if chr.upper == "X":
-                self.cnt += 20
-                continue
+                if symbol.upper() == "X":
+                    self.cnt += 20
+                    continue
 
-            if chr == "/":
-                self.cnt = self.cnt - int(self.game_result[i - 1]) + 15
-                continue
+                if symbol == "/":
+                    self.cnt = self.cnt - int(self.game_result[i - 1]) + 15
+                    continue
 
-            else:
-                raise Exception
+                else:
+                    raise VsePoJope
+
+            except Exception as exc:
+                raise VsePoJope
+
+        return self.cnt
+
+
+if __name__ == '__main__':
+    baton = BowlCntr("baton")
+    print(baton.get_score("///34"))
---
+++ lesson 14/tests/test.py
@@ -0,0 +1,4 @@
+from Main import BowlCntr as BowlCntr
+
+gvozd = BowlCntr("gvozd")
+assert gvozd.get_score("X4/34") == 42, "Шото хуйня"
