41a0a22e326dfaa9437688c20978d4118f7847bf
Владимир <7572vova@mail.ru>
2023-10-16 16:18:05 +0400

    Сам хз что наделал с прошлого коммита, начал заниматься юниттестами....))

--- lesson 13/VK-bot/main.py
+++ lesson 13/VK-bot/main.py
@@ -1,25 +1,56 @@
 from vk_api import VkApi, bot_longpoll
 from auth_data import auth_key, group_id
 from random import randint
+import logging
 
+class Fatality(Exception):
+
+    def __str__(self):
+        return "Screw u guys, Im gona home"
+
+
+logger = logging.getLogger('Inf')
+logger.setLevel('DEBUG')
+
+s_handler = logging.StreamHandler()
+s_handler.setLevel(logging.INFO)
+
+f_handler = logging.FileHandler(filename='errors.txt', mode='a')
+f_handler.setLevel(logging.WARNING)
+
+formatter = logging.Formatter("%(levelname)s - %(asctime)s — %(message)s")
+s_handler.setFormatter(formatter)
+f_handler.setFormatter(formatter)
+
+logger.addHandler(s_handler)
+logger.addHandler(f_handler)
 
 class EchoBot:
 
     def __init__(self, key, id):
         self.vk_src = VkApi(token=key)
+        logger.info('Получен ключ')
+        logger.warning('в жопу кайла')
         self.bot = bot_longpoll.VkBotLongPoll(self.vk_src, id)
         self.sender = self.vk_src.get_api()
 
     def run(self, timeout=25):
-        for event in self.bot.listen():
+        try:
+            for event in self.bot.listen():
 
-            if event.type == bot_longpoll.VkBotEventType.MESSAGE_NEW:
-                #     print(f'Новое сообщение от {event.object}, текст сообщения: \n')
-                print(event.message.from_id, ' ', event.message.text)
-                self.sender.messages.send(user_id=event.message.from_id, random_id=randint(0, 999),
-                                          message=event.message.text + ' ' + event.message.text)
+                if event.type == bot_longpoll.VkBotEventType.MESSAGE_NEW:
+                    if event.message.text == 'Fatality':
+                        raise Fatality
+                    #     print(f'Новое сообщение от {event.object}, текст сообщения: \n')
+                    print(event.message.from_id, ' ', event.message.text)
+                    self.sender.messages.send(user_id=event.message.from_id, random_id=randint(0, 999),
+                                              message=event.message.text + ' ' + event.message.text)
+
+        except Exception as err:
+            logger.error(err)
 
 
 if __name__ == '__main__':
+    logger.debug('STart')
     bot = EchoBot(auth_key, group_id)
     bot.run()
--- lesson 14/Main.py
+++ lesson 14/Main.py
@@ -1,3 +1,8 @@
+import logging
+from logging import Formatter
+
+
+
 class VsePoJope(Exception):
 
     def __str__(self):
@@ -13,29 +18,44 @@ class BowlCntr:
     def get_score(self, inpt):
         self.game_result = inpt
 
+        log.info('hmmmm....info')
+        log.debug('hmmmm....debug')
+        log.warning('hmmmm....warn')
+        log.error('hmmmm....error')
+
         for i, symbol in enumerate(self.game_result):
-            try:
-                if symbol.isdigit():
-                    self.cnt += int(symbol)
-                    continue
+            # try:
 
-                if symbol.upper() == "X":
-                    self.cnt += 20
-                    continue
 
-                if symbol == "/":
-                    self.cnt = self.cnt - int(self.game_result[i - 1]) + 15
-                    continue
 
-                else:
-                    raise VsePoJope
+            if symbol.isdigit():
+                self.cnt += int(symbol)
+                continue
 
-            except Exception as exc:
+            if symbol.upper() == "X":
+                self.cnt += 20
+                continue
+
+            if symbol == "/":
+                self.cnt = self.cnt - int(self.game_result[i - 1]) + 15
+                continue
+
+            else:
                 raise VsePoJope
+
+        # except Exception as exc:
+        #     raise VsePoJope
 
         return self.cnt
 
 
 if __name__ == '__main__':
+    log = logging.getLogger('__name__')
+    log.setLevel(logging.DEBUG)
+    shell_handler = logging.StreamHandler()
+    shell_handler.setLevel('WARNING')
+    shell_handler.setFormatter(Formatter(fmt = '%(asctime)s     %(funcName)s    %(levelname)s:%(name)s:%(message)s'))
+    log.addHandler(shell_handler)
+
     baton = BowlCntr("baton")
-    print(baton.get_score("///34"))
+    print(baton.get_score("1/134"))
